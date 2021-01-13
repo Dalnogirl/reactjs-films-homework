916521c5174d49af2d8549dbae000b38decff2e1
Dalnogirl <velikiizevs@gmail.com>
2021-01-13 14:52:55 +0300

    feat: add config with sass, hotreload

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const webpack = require("webpack");
+
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const htmlPlugin = new HtmlWebPackPlugin({
+    template: "./src/index.html",
+    filename: "./index.html"
+});
+module.exports = {
+    entry: ["./src/index.js", 'webpack-hot-middleware/client'],
+    output: { // NEW
+        path: path.join(__dirname, 'dist'),
+        filename: "[name].js"
+    }, // NEW Ends
+    plugins: [htmlPlugin,
+        new webpack.HotModuleReplacementPlugin(),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader', options: {modules: true,}
+                    },
+                    "sass-loader",
+                ],
+            },
+        ],
+    },
+
+
+}
