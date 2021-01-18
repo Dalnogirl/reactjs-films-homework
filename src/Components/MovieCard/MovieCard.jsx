e943f7735ccf308789116e5e25d36b30576dd5df
Dalnogirl <velikiizevs@gmail.com>
2021-01-18 16:24:40 +0300

    fix: movieCard hover changed

--- src/Components/MovieCard/MovieCard.jsx
+++ src/Components/MovieCard/MovieCard.jsx
@@ -6,44 +6,42 @@ import MovieTags from '../utils/MovieTags/MovieTags'
 import Rating from '../utils/Rating/Rating'
 
 const MovieCard = React.memo(({movieName, rating, movieTags, poster, overview, genres}) => {
-    let [viewInfoMode, setViewInfoMode] = useState(false)
-    if (movieName.length > 14) movieName = movieName.slice(0,14) + '...'
-    return viewInfoMode
-        ? <div className={styles.movieCard}>
-            <div className={styles.overviewMode}>
-                {overview}
-                <Button width={100} onClick={()=>{setViewInfoMode(false)}}>Go Back</Button>
+    if (movieName.length > 14) movieName = movieName.slice(0, 14) + '...'
+    let [hover, setHover] = useState(false)
+    let [infoMode, setInfoMode] = useState(false)
+
+    return (
+        <div className={styles.card}>
+            <div className={styles.poster}
+                 onMouseEnter={() => setHover(true)}
+
+                 style={{backgroundImage: `url(https://image.tmdb.org/t/p/original${poster})`}}>
+                {hover &&
+                <div onMouseLeave={() => {
+                    setHover(false)
+                    setInfoMode(false)
+                }} className={styles.hoverInfo}>
+                    {infoMode ? <div className={styles.overview}>
+                            {overview}
+                        </div>
+                        : <div>
+                            <img src={play} alt="" className={styles.playButton}/>
+                            <Button onClick={() => setInfoMode(true)}>
+                                View Info
+                            </Button>
+                        </div>}
+                </div>}
             </div>
+
             <footer className={styles.footer}>
-                <div className={styles.movieNameAndRating}>
-                    <p className={styles.movieName}>
-                        {movieName}
-                    </p>
-                    <Rating starsCount={rating}/>
-                </div>
-                <MovieTags movieTags={movieTags}/>
-            </footer>
-        </div>
-        :<div className={styles.movieCard}>
-            <div className={styles.smallPoster} style={{backgroundImage: `url(https://image.tmdb.org/t/p/original${poster})`}}>
-                <div className={styles.onHoverInfo}>
-                    <img src={play} alt="" className={styles.playButton}/>
-                    <Button onClick={()=>setViewInfoMode(true)}>
-                        View Info
-                    </Button>
-                </div>
-            </div>
-            <footer className={styles.footer}>
-                <div className={styles.movieNameAndRating}>
-                    <p className={styles.movieName}>
-                        {movieName}
-                    </p>
+                <div className={styles.nameAndRating}>
+                    <p className={styles.name}>{movieName}</p>
                     <Rating starsCount={rating}/>
                 </div>
                 <MovieTags movieTags={movieTags} genres={genres}/>
             </footer>
         </div>
-
+    )
 })
 
 export default MovieCard
