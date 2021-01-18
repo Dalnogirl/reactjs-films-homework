f22d3653a2058416fb115ee126d2343f5ced9040
Dalnogirl <velikiizevs@gmail.com>
2021-01-18 12:53:34 +0300

    fix: use of props.children

--- src/Components/utils/Button/Button.js
+++ src/Components/utils/Button/Button.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import styles from './Button.module.scss'
 
-const Button = ({text, onClick}) => (
+const Button = (props) => (
     <div className={styles.button}
-         onClick={onClick}>
-        {text}
+         style={{width:props.width}}
+         onClick={props.onClick}>
+        {props.children}
     </div>
 )
 
