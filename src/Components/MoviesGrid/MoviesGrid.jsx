14443d993180d9a83f085bb53ae7747e01b2411c
Dalnogirl <velikiizevs@gmail.com>
2021-01-18 20:49:48 +0300

    fix: getting callback from props

--- src/Components/MoviesGrid/MoviesGrid.jsx
+++ src/Components/MoviesGrid/MoviesGrid.jsx
@@ -2,16 +2,16 @@ import React, {useEffect, useRef, useState} from 'react'
 import styles from './MoviesGrid.module.scss'
 import MovieCard from '../MovieCard/MovieCard'
 import {useDispatch, useSelector} from 'react-redux'
-import {setGenresArray, setTopRatedMovies} from '../../redux/moviesReducer'
+import {setGenresArray, setPopularMovies, setTopRatedMovies} from '../../redux/moviesReducer'
 import {getGenresSelector, getMoviesSelector} from '../../redux/selectors/selectors'
 import Loader from '../utils/Loader/Loader'
 
-const MoviesGrid = ({}) => {
+const MoviesGrid = ({setMovies}) => {
     const dispatch = useDispatch()
 
 
     useEffect(() => {
-        dispatch(setTopRatedMovies(1))
+        dispatch(setMovies(1))
         dispatch(setGenresArray())
         let options = {
             root: null,
@@ -19,6 +19,7 @@ const MoviesGrid = ({}) => {
             threshold: 1.0
         }
         const handleObserver = (entities) => {
+            debugger
             const target = entities[0]
             if (target.isIntersecting) {
                 setPage((page) => page + 1)
@@ -30,7 +31,6 @@ const MoviesGrid = ({}) => {
         }
     }, [])
 
-
     let list = useSelector(getMoviesSelector)
     let genres = useSelector(getGenresSelector)
 
@@ -38,36 +38,21 @@ const MoviesGrid = ({}) => {
 
     const loader = useRef(null)
 
-
-    // useEffect(() => {
-    //
-    //     let options = {
-    //         root: null,
-    //         rootMargin: '20px',
-    //         threshold: 1.0
-    //     }
-    //     const observer = new IntersectionObserver(handleObserver, options)
-    //     if (loader.current) {
-    //         observer.observe(loader.current)
-    //     }
-    // }, [])
-
     useEffect(() => {
-        dispatch(setTopRatedMovies(page))
+        dispatch(setMovies(page))
         window.scrollTo({
             top: 750,
             behavior: 'smooth'
         })
     }, [page])
 
-
     return !list ? <div className={styles.container}>
             <Loader/>
         </div>
         : <div className={styles.container}>
-            <div>Top Rated</div>
             <div className={styles.moviesGrid}>
                 {list.results.map((item) => <MovieCard key={item.id}
+                                                       id={item.id}
                                                        genres={genres}
                                                        movieName={item.title}
                                                        movieTags={item.genre_ids}
