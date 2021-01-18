6eec517c85666e00fa47eeabe2df2c28841ff930
Dalnogirl <velikiizevs@gmail.com>
2021-01-18 20:46:48 +0300

    feat: add react router

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -2,20 +2,36 @@ import React from 'react'
 import {hot} from 'react-hot-loader'
 import styles from './App.scss'
 import MovieDetailsPage from '../MovieDetailsPage/MovieDetailsPage'
-import MoviesGrid from '../MoviesGrid/MoviesGrid'
 import {Provider} from 'react-redux'
 import store from '../../redux/store'
+import {HashRouter, Route} from 'react-router-dom'
+import MoviesGrid from '../MoviesGrid/MoviesGrid'
+import Navbar from '../Navbar/Navbar'
+import {setPopularMovies, setTopRatedMovies} from '../../redux/moviesReducer'
 
 
-const App = () => (
-    <Provider store={store}>
-        <div className={styles.app}>
-            <MovieDetailsPage/>
-            <MoviesGrid/>
-        </div>
-    </Provider>
+const App = () => {
 
-)
+
+    return (
+        <Provider store={store}>
+            <HashRouter>
+                <div className={styles.app}>
+                    <Route path={[
+                        '/popular/:movieId?',
+                        '/topRated/:movieId?'
+                    ]} render={() => <MovieDetailsPage/>}/>
+                    <main className={styles.main}>
+                        <Navbar/>
+                        <Route path='/popular' render={() => <MoviesGrid setMovies={setPopularMovies}/>}/>
+                        <Route path='/topRated' render={() => <MoviesGrid setMovies={setTopRatedMovies}/>}/>
+                    </main>
+                </div>
+            </HashRouter>
+        </Provider>
+
+    )
+}
 
 
 export default hot(module)(App)
