322f3380e990e7acc64b1c97e45ab2aa9a9bdbc1
Dalnogirl <velikiizevs@gmail.com>
2021-01-19 21:08:32 +0300

    feat: useLocation hook usage

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -4,34 +4,42 @@ import styles from './App.scss'
 import MovieDetailsPage from '../MovieDetailsPage/MovieDetailsPage'
 import {Provider} from 'react-redux'
 import store from '../../redux/store'
-import {HashRouter, Route} from 'react-router-dom'
+import {HashRouter, useLocation} from 'react-router-dom'
 import MoviesGrid from '../MoviesGrid/MoviesGrid'
 import Navbar from '../Navbar/Navbar'
 import {setPopularMovies, setTopRatedMovies} from '../../redux/moviesReducer'
 
 
 const App = () => {
-
+    const location = useLocation()
+    let movieId = location.pathname.match(/(\d+)$/gmi) ? location.pathname.match(/(\d+)$/gmi)[0] : 123
+    let gridFilter = location.pathname.match(/^\/(\w+)/gi)[0]
+    let setMovieCallbacks = {
+        '/popular': setPopularMovies,
+        '/topRated': setTopRatedMovies
+    }
+    let setMovieCallback = setMovieCallbacks[gridFilter]
 
     return (
-        <Provider store={store}>
-            <HashRouter>
-                <div className={styles.app}>
-                    <Route path={[
-                        '/popular/:movieId?',
-                        '/topRated/:movieId?'
-                    ]} render={() => <MovieDetailsPage/>}/>
-                    <main className={styles.main}>
-                        <Navbar/>
-                        <Route path='/popular' render={() => <MoviesGrid setMovies={setPopularMovies}/>}/>
-                        <Route path='/topRated' render={() => <MoviesGrid setMovies={setTopRatedMovies}/>}/>
-                    </main>
-                </div>
-            </HashRouter>
-        </Provider>
-
+        <div className={styles.app}>
+            <MovieDetailsPage movieId={movieId}/>
+            <main className={styles.main}>
+                <Navbar/>
+                <MoviesGrid setMovies={setMovieCallback}/>
+            </main>
+        </div>
     )
 }
 
+const AppContainer = () => (
+    <Provider store={store}>
+        <HashRouter>
+            <App>
+            </App>
+        </HashRouter>
+    </Provider>
 
-export default hot(module)(App)
+)
+
+
+export default hot(module)(AppContainer)
