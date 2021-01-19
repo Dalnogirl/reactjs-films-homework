10464df9b30e0eae89de52c22110ee45e7b1e9c0
Dalnogirl <velikiizevs@gmail.com>
2021-01-19 21:09:41 +0300

    feat: usage of reselect (!BROKEN)

--- src/redux/selectors/selectors.js
+++ src/redux/selectors/selectors.js
@@ -1,5 +1,14 @@
+import {createSelector} from 'reselect'
+
 export let getMovieInfoSelector = (state) => state.headerData.movieInfo
 
 export let getMoviesSelector = state => state.moviesData.moviesList
 
 export let getGenresSelector = state => state.moviesData.genresArray
+
+export let getGenresNames = createSelector(getGenresSelector,
+    genresArray => {
+
+        return genresArray.map(i => i.name)
+    }
+)
