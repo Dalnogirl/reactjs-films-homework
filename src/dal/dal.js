d06c9f71c0b4838aa4dad6cfb8722c32ca60dd01
Dalnogirl <velikiizevs@gmail.com>
2021-01-25 17:48:11 +0300

    feat: add trailer AJAX

--- src/dal/dal.js
+++ src/dal/dal.js
@@ -1,23 +1,21 @@
 import axios from 'axios'
 
-let axiosInstance = axios.create({
-    baseURL: 'https://api.themoviedb.org/3',
+const axiosInstance = axios.create({
+  baseURL: 'https://api.themoviedb.org/3',
 })
 
-export let headerAPI = {
-    getMovieInfo: (movieId) => {
-        return axiosInstance.get(`/movie/${movieId}?api_key=0d62501dce3049a65b9d183d8e927cfa`).then(response => response.data)
-    },
+export const headerAPI = {
+  getMovieInfo: (movieId) => axiosInstance.get(`/movie/${movieId}?api_key=0d62501dce3049a65b9d183d8e927cfa`)
+    .then((response) => response.data),
+  getTrailerKey: (movieId) => axiosInstance.get(`/movie/${movieId}/videos?api_key=0d62501dce3049a65b9d183d8e927cfa`)
+    .then((response) => response.data),
 
 }
 
-export let moviesApi = {
-    getGenresObj: () => axiosInstance.get('/genre/movie/list?api_key=0d62501dce3049a65b9d183d8e927cfa')
-        .then(response => response.data)
+export const moviesApi = {
+  getGenresObj: () => axiosInstance.get('/genre/movie/list?api_key=0d62501dce3049a65b9d183d8e927cfa')
+    .then((response) => response.data),
 
-    ,
-    getMovies: getUrl => axiosInstance.get(getUrl)
-        .then(res => res.data)
+  getMovies: (getUrl) => axiosInstance.get(getUrl)
+    .then((res) => res.data),
 }
-
-
