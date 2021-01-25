3fd712864d0788331472a4b3f6c6881e4eb933d9
Dalnogirl <velikiizevs@gmail.com>
2021-01-25 14:27:35 +0300

    fix: fixed code duplication

--- src/dal/dal.js
+++ src/dal/dal.js
@@ -1,34 +1,23 @@
 import axios from 'axios'
 
-
 let axiosInstance = axios.create({
     baseURL: 'https://api.themoviedb.org/3',
-    // headers: {
-    //     'API-KEY': '0d62501dce3049a65b9d183d8e927cfa'
-    // }
 })
 
 export let headerAPI = {
     getMovieInfo: (movieId) => {
         return axiosInstance.get(`/movie/${movieId}?api_key=0d62501dce3049a65b9d183d8e927cfa`).then(response => response.data)
-    }
+    },
+
 }
 
 export let moviesApi = {
-    getTopRatedMovies: (page) => (
-        axiosInstance.get(`/movie/top_rated?api_key=0d62501dce3049a65b9d183d8e927cfa&page=${page}`)
-            .then(response => response.data)
-    ),
-    getPopularMovies:(page) => (
-        axiosInstance.get(`/movie/popular?api_key=0d62501dce3049a65b9d183d8e927cfa&page=${page}`)
-            .then(response => response.data)
-    ),
-     getGenresArray: () => {
-         return (
-             axiosInstance.get('/genre/movie/list?api_key=0d62501dce3049a65b9d183d8e927cfa')
-                 .then(response => response.data)
-         )
-     }
+    getGenresObj: () => axiosInstance.get('/genre/movie/list?api_key=0d62501dce3049a65b9d183d8e927cfa')
+        .then(response => response.data)
+
+    ,
+    getMovies: getUrl => axiosInstance.get(getUrl)
+        .then(res => res.data)
 }
 
 
