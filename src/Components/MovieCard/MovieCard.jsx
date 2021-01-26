e695a5009b241390cec74a49402fab227be5086d
Dalnogirl <velikiizevs@gmail.com>
2021-01-26 16:08:47 +0300

    feat: added button for trailer viewing

--- src/Components/MovieCard/MovieCard.jsx
+++ src/Components/MovieCard/MovieCard.jsx
@@ -8,46 +8,52 @@ import {Link, useLocation} from 'react-router-dom'
 import {urlCreatorForCard} from '../utils/functions/functions'
 
 const MovieCard = (({movieName, rating, genresIds, poster, overview, id, allGenres}) => {
-    if (movieName.length > 14) movieName = movieName.slice(0, 14) + '...'
-    let [hover, setHover] = useState(false)
-    let [infoMode, setInfoMode] = useState(false)
-    const location = useLocation()
-    let link = urlCreatorForCard(location, id) // no id in current url, so we should pass id as argument
+  if (movieName.length > 14) movieName = movieName.slice(0, 14) + '...'
+  let [hover, setHover] = useState(false)
+  let [infoMode, setInfoMode] = useState(false)
+  const location = useLocation()
+  let link = urlCreatorForCard(location, id) // no id in current url, so we should pass id as argument
+  console.log(link)
 
-    return (
+  return (
+      <div className={styles.cardContainer}>
         <div className={styles.card}>
-            <div className={styles.poster}
-                 onMouseEnter={() => setHover(true)}
-                 style={{backgroundImage: `url(https://image.tmdb.org/t/p/original${poster})`}}>
-                {hover &&
-                <div onMouseLeave={() => {
-                    setHover(false)
-                    setInfoMode(false)
-                }} className={styles.hoverInfo}>
-                    {infoMode ? <div className={styles.overview}>
-                            {overview}
-                        </div>
-                        : <div>
-                            <img src={play} alt="" className={styles.playButton}/>
-                            <Button onClick={() => setInfoMode(true)}>
-                                View Info
-                            </Button>
-                        </div>}
-                </div>}
+          <div className={styles.poster}
+               onMouseEnter={() => setHover(true)}
+               style={{backgroundImage: `url(https://image.tmdb.org/t/p/original${poster})`}}>
+            {hover &&
+            <div onMouseLeave={() => {
+              setHover(false)
+              setInfoMode(false)
+            }} className={styles.hoverInfo}>
+
+              {infoMode ? <div className={styles.overview}>
+                    {overview}
+                  </div>
+                  : <div>
+                    <Link to={`${link}/trailer`}><img src={play} alt=""
+                                                      className={styles.playButton}/></Link>
+                    <Button onClick={() => setInfoMode(true)}>
+                      View Info
+                    </Button>
+                  </div>}
+
+            </div>}
+          </div>
+
+          <Link to={link} className={styles.footer}>
+            <div className={styles.nameAndRating}>
+              <div className={styles.name}
+              >{movieName}</div>
+
+              <Rating starsCount={rating}/>
             </div>
-
-            <Link to={link} className={styles.footer}>
-                <div className={styles.nameAndRating}>
-                    <div className={styles.name}
-                    >{movieName}</div>
-
-                    <Rating starsCount={rating}/>
-                </div>
-                <MovieTags allGenres={allGenres}
-                           genresIds={genresIds}/>
-            </Link>
+            <MovieTags allGenres={allGenres}
+                       genresIds={genresIds}/>
+          </Link>
         </div>
-    )
+      </div>
+  )
 })
 
 export default React.memo(MovieCard)
