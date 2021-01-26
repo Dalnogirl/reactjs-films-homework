f0104ae1298956f96a5b518956106f7f4961a1e2
Dalnogirl <velikiizevs@gmail.com>
2021-01-26 16:09:19 +0300

    fix: routing for trailer

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,44 +1,51 @@
 import React from 'react'
 import {hot} from 'react-hot-loader'
+import {Provider} from 'react-redux'
+import {BrowserRouter, Redirect, Route} from 'react-router-dom'
 import styles from './App.scss'
 import MovieDetailsPage from '../MovieDetailsPage/MovieDetailsPage'
-import {Provider} from 'react-redux'
 import store from '../../redux/store'
-import {BrowserRouter, Redirect, Route} from 'react-router-dom'
 import MoviesGrid from '../MoviesGrid/MoviesGrid'
 import Navbar from '../Navbar/Navbar'
 import Input from '../utils/Input/Input'
+import Trailer from '../Trailer/Trailer'
 
-
-const App = () => {
-    return (
-        <div className={styles.app}>
-            <Route exact path='/' render={() => <Redirect to={`/filter=topRated`}/>}/>
-            <header className={styles.header}>
-                <h1 className={styles.logo}>FILMS</h1>
-                <div className={styles.searchContainer}>
-                    <Input type="text" placeholder={'Search'}/>
-                </div>
-            </header>
-            <Route path={[ '/search?q=:search&page=:page&id=:id',
-                '/filter=:filterName&page=:page&id=:id']} render={() => <MovieDetailsPage/>}/>
-            <main className={styles.main}>
-                <Navbar/>
-                <Route path={'/'} render={() => <MoviesGrid/>}/>
-            </main>
+const App = () => (
+    <div className={styles.app}>
+      <Route exact path="/"
+             render={() => <Redirect to="/filter=top_rated&page=1"/>}/>
+      <Route path={'/**/trailer'}
+             render={() => <Trailer/>}
+      />
+      <header className={styles.header}>
+        <h1 className={styles.logo}>FILMS</h1>
+        <div className={styles.searchContainer}>
+          <Input type="text" placeholder="Search"/>
         </div>
-    )
-}
+      </header>
+      <Route
+          path={[
+            '/search?q=:search&page=:page&id=:id',
+            '/**&id=:id']}
+          render={() => {
+            console.log('details rendered')
+            return <MovieDetailsPage/>}}
+      />
+
+      <main className={styles.main}>
+        <Navbar/>
+        <Route path="/" render={() => <MoviesGrid/>}/>
+      </main>
+    </div>
+)
 
 const AppContainer = () => (
     <Provider store={store}>
-        <BrowserRouter>
-            <App>
-            </App>
-        </BrowserRouter>
+      <BrowserRouter>
+        <App/>
+      </BrowserRouter>
     </Provider>
 
 )
-
 
 export default hot(module)(AppContainer)
