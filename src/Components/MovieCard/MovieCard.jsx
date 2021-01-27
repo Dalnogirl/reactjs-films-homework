3d1cc90e43eaea7dab858973c41e09d8b8afc8ac
Dalnogirl <velikiizevs@gmail.com>
2021-01-27 20:08:52 +0300

    fix: props reassign fixed

--- src/Components/MovieCard/MovieCard.jsx
+++ src/Components/MovieCard/MovieCard.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import {Link, useLocation} from 'react-router-dom'
 import styles from './MovieCard.module.scss'
 import play from '../../assets/play (1).svg'
@@ -11,8 +11,10 @@ import Modal from '../utils/Modal/Modal'
 const MovieCard = (({
   movieName, rating, genresIds, poster, overview, id, allGenres,
 }) => {
-  if (movieName.length > 14) movieName = `${movieName.slice(0, 14)}...`
-  const [hover, setHover] = useState(false)
+  const [movieTitle, setMovieTitle] = useState(movieName)
+  useEffect(() => {
+    if (movieName.length > 14) setMovieTitle(`${movieName.slice(0, 14)}...`)
+  }, [])
   const [infoMode, setInfoMode] = useState(false)
   const location = useLocation()
   const link = urlCreatorForCard(location, id)
@@ -23,56 +25,49 @@ const MovieCard = (({
         <div className={styles.card}>
           <div
               className={styles.poster}
-              onMouseEnter={() => setHover(true)}
               style={{backgroundImage: `url(https://image.tmdb.org/t/p/original${poster})`}}
           >
-            {hover
-            && (
-                <div
-                    onMouseLeave={() => {
-                      setHover(false)
-                      setInfoMode(false)
-                    }}
-                    className={styles.hoverInfo}
-                >
-                  {infoMode
-                      ? (
-                          <div className={styles.overview}>
-                            {overview}
-                          </div>
-                      )
-                      : (
-                          <div>
-                            <div onClick={() => { setIsTrailerVisible(true) }}>
-                              <img
-                                  src={play}
-                                  alt=""
-                                  className={styles.playButton}
+            <div
+                onMouseLeave={() => { setInfoMode(false) }}
+                className={styles.hoverInfo}
+            >
+              {infoMode
+                  ? (
+                      <div className={styles.overview}>
+                        {overview}
+                      </div>
+                  )
+                  : (
+                      <div>
+                        <div onClick={() => { setIsTrailerVisible(true) }}>
+                          <img
+                              src={play}
+                              alt=""
+                              className={styles.playButton}
+                          />
+                        </div>
+                        {
+                          isTrailerVisible
+                          && (
+                              <Modal
+                                  callback={setIsTrailerVisible}
+                                  movieId={id}
                               />
-                            </div>
-                            {
-                              isTrailerVisible
-                              && (
-                                  <Modal
-                                      callback={setIsTrailerVisible}
-                                      movieId={id}
-                                  />
-                              )
-                            }
-                            <Button onClick={() => setInfoMode(true)}>
-                              View Info
-                            </Button>
-                          </div>
-                      )}
+                          )
+                        }
+                        <Button onClick={() => setInfoMode(true)}>
+                          View Info
+                        </Button>
+                      </div>
+                  )}
 
-                </div>
-            )}
+            </div>
           </div>
 
           <Link to={link} className={styles.footer}>
             <div className={styles.nameAndRating}>
               <div className={styles.name}>
-                {movieName}
+                {movieTitle}
               </div>
               <Rating starsCount={rating}/>
             </div>
