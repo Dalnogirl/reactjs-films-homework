f0dd5881348d07ec2fc97640d52bafe833bb05cf
Dalnogirl <velikiizevs@gmail.com>
2021-01-27 20:07:06 +0300

    fix: delete spaces

--- src/index.js
+++ src/index.js
@@ -2,6 +2,4 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './Components/App/App'
 
-
-// ReactDOM.render(<Rate disabled defaultValue={2}/>, document.getElementById('app'))
  ReactDOM.render(<App/>, document.getElementById('app'))
