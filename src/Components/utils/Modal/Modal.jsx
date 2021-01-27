8997c7555688bf88a10469937e47c26f886d5027
Dalnogirl <velikiizevs@gmail.com>
2021-01-27 18:54:43 +0300

    feat: add trailer modal

---
+++ src/Components/utils/Modal/Modal.jsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+import Trailer from '../../Trailer/Trailer'
+
+const Modal = ({callback, movieId}) => ReactDOM.createPortal(
+    <Trailer callback={callback} movieId={movieId}/>,
+    document.querySelector('#app'))
+
+export default Modal
