dbdaf35ad855f584c4a59a2332bb683533915e91
Dalnogirl <velikiizevs@gmail.com>
2021-01-27 20:00:39 +0300

    fix: public path

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -7,7 +7,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 module.exports = merge([base, {
     mode: 'production',
     output: {
-
+        publicPath: '/',
     },
     plugins: [
         new CleanWebpackPlugin,
