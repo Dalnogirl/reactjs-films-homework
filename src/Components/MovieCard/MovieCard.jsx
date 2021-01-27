800ea1d864d9df5bf1850cbd96f7071c5daf4579
Dalnogirl <velikiizevs@gmail.com>
2021-01-27 18:56:02 +0300

    feat: usage of modal trailer

--- src/Components/MovieCard/MovieCard.jsx
+++ src/Components/MovieCard/MovieCard.jsx
@@ -1,55 +1,85 @@
 import React, {useState} from 'react'
+import {Link, useLocation} from 'react-router-dom'
 import styles from './MovieCard.module.scss'
 import play from '../../assets/play (1).svg'
 import Button from '../utils/Button/Button'
 import MovieTags from '../utils/MovieTags/MovieTags'
 import Rating from '../utils/Rating/Rating'
-import {Link, useLocation} from 'react-router-dom'
 import {urlCreatorForCard} from '../utils/functions/functions'
+import Modal from '../utils/Modal/Modal'
 
-const MovieCard = (({movieName, rating, genresIds, poster, overview, id, allGenres}) => {
-  if (movieName.length > 14) movieName = movieName.slice(0, 14) + '...'
-  let [hover, setHover] = useState(false)
-  let [infoMode, setInfoMode] = useState(false)
+const MovieCard = (({
+  movieName, rating, genresIds, poster, overview, id, allGenres,
+}) => {
+  if (movieName.length > 14) movieName = `${movieName.slice(0, 14)}...`
+  const [hover, setHover] = useState(false)
+  const [infoMode, setInfoMode] = useState(false)
   const location = useLocation()
-  let link = urlCreatorForCard(location, id) // no id in current url, so we should pass id as argument
-  console.log(link)
+  const link = urlCreatorForCard(location, id)
+  const [isTrailerVisible, setIsTrailerVisible] = useState(false)
 
   return (
       <div className={styles.cardContainer}>
         <div className={styles.card}>
-          <div className={styles.poster}
-               onMouseEnter={() => setHover(true)}
-               style={{backgroundImage: `url(https://image.tmdb.org/t/p/original${poster})`}}>
-            {hover &&
-            <div onMouseLeave={() => {
-              setHover(false)
-              setInfoMode(false)
-            }} className={styles.hoverInfo}>
+          <div
+              className={styles.poster}
+              onMouseEnter={() => setHover(true)}
+              style={{backgroundImage: `url(https://image.tmdb.org/t/p/original${poster})`}}
+          >
+            {hover
+            && (
+                <div
+                    onMouseLeave={() => {
+                      setHover(false)
+                      setInfoMode(false)
+                    }}
+                    className={styles.hoverInfo}
+                >
+                  {infoMode
+                      ? (
+                          <div className={styles.overview}>
+                            {overview}
+                          </div>
+                      )
+                      : (
+                          <div>
+                            <div onClick={() => { setIsTrailerVisible(true) }}>
+                              <img
+                                  src={play}
+                                  alt=""
+                                  className={styles.playButton}
+                              />
+                            </div>
+                            {
+                              isTrailerVisible
+                              && (
+                                  <Modal
+                                      callback={setIsTrailerVisible}
+                                      movieId={id}
+                                  />
+                              )
+                            }
+                            <Button onClick={() => setInfoMode(true)}>
+                              View Info
+                            </Button>
+                          </div>
+                      )}
 
-              {infoMode ? <div className={styles.overview}>
-                    {overview}
-                  </div>
-                  : <div>
-                    <Link to={`${link}/trailer`}><img src={play} alt=""
-                                                      className={styles.playButton}/></Link>
-                    <Button onClick={() => setInfoMode(true)}>
-                      View Info
-                    </Button>
-                  </div>}
-
-            </div>}
+                </div>
+            )}
           </div>
 
           <Link to={link} className={styles.footer}>
             <div className={styles.nameAndRating}>
-              <div className={styles.name}
-              >{movieName}</div>
-
+              <div className={styles.name}>
+                {movieName}
+              </div>
               <Rating starsCount={rating}/>
             </div>
-            <MovieTags allGenres={allGenres}
-                       genresIds={genresIds}/>
+            <MovieTags
+                allGenres={allGenres}
+                genresIds={genresIds}
+            />
           </Link>
         </div>
       </div>
--- src/Components/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/Components/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,29 +1,32 @@
 import React, {useEffect, useState} from 'react'
+import {useDispatch, useSelector} from 'react-redux'
+import {useLocation} from 'react-router-dom'
+import ReactDOM from 'react-dom'
 import styles from './MovieDetailsPage.module.scss'
 import Rating from '../utils/Rating/Rating'
 import Popover from '../utils/Popover/Popover'
 import MovieTags from '../utils/MovieTags/MovieTags'
-import {useDispatch, useSelector} from 'react-redux'
-import {getMovieInfo, setTrailerKey} from '../../redux/headerReducer'
+import {getMovieInfo} from '../../redux/headerReducer'
 import {
   getIsHeaderFetching,
   getMovieInfoSelector,
-  getTrailerKey,
 } from '../../redux/selectors/selectors'
-import {Link, useLocation} from 'react-router-dom'
 import Loader from '../utils/Loader/Loader'
 import {urlCreatorForCard, urlHelpers} from '../utils/functions/functions'
+import Trailer from '../Trailer/Trailer'
 import Button from '../utils/Button/Button'
+import Modal from '../utils/Modal/Modal'
 
 const MovieDetailsPage = () => {
-  let dispatch = useDispatch()
-  let location = useLocation()
-  let isHeaderFetching = useSelector(getIsHeaderFetching)
-  let id = urlHelpers.getId(location)
-  let link = urlCreatorForCard(location, id) // no id in current url, so we should pass id as argument
+  const dispatch = useDispatch()
+  const location = useLocation()
+  const isHeaderFetching = useSelector(getIsHeaderFetching)
+  const id = urlHelpers.getId(location)
+  const link = urlCreatorForCard(location, id)
+  const [isTrailerVisible, setIsTrailerVisible] = useState(false)
 
   useEffect(() => {
-    let movieId = urlHelpers.getId(location)
+    const movieId = urlHelpers.getId(location)
     if (movieId) {
       dispatch(getMovieInfo(movieId))
       window.scrollTo({
@@ -33,31 +36,40 @@ const MovieDetailsPage = () => {
     }
   }, [location])
 
-  let movieInfo = useSelector(getMovieInfoSelector)
+  const movieInfo = useSelector(getMovieInfoSelector)
 
-  return !isHeaderFetching && movieInfo ? <div className={styles.container}
-                                               style={{backgroundImage: `url(https://image.tmdb.org/t/p/original${movieInfo.backdrop_path})`}}>
-    <main className={styles.main}>
-      <div className={styles.mainInfoWrapper}>
-        <p className={styles.movieName}>
-          {movieInfo.title}
-        </p>
-        <div className={styles.movieTagContainer}>
-          <MovieTags genresNames={movieInfo.genres}/>
-        </div>
-        <Rating starsCount={movieInfo.vote_average / 2}/>
-      </div>
-      <div className={styles.controls}>
-        <Link to={`${link}/trailer`}>
-          <Button>Watch trailer</Button>
-        </Link>
-        <Popover width={70} text={`Info`}>
-          {movieInfo.overview}
-        </Popover>
-      </div>
+  return id ? !isHeaderFetching && movieInfo ? (
+          <div
+              className={styles.container}
+              style={{backgroundImage: `url(https://image.tmdb.org/t/p/original${movieInfo.backdrop_path})`}}
+          >
+            <main className={styles.main}>
+              <div className={styles.mainInfoWrapper}>
+                <p className={styles.movieName}>
+                  {movieInfo.title}
+                </p>
+                <div className={styles.movieTagContainer}>
+                  <MovieTags genresNames={movieInfo.genres}/>
+                </div>
+                <Rating starsCount={movieInfo.vote_average / 2}/>
+              </div>
+              <div className={styles.controls}>
+                <Button onClick={() => {
+                  setIsTrailerVisible(true)
+                }}
+                >
+                  Watch Trailer
+                </Button>
+                {isTrailerVisible && <Modal callback={setIsTrailerVisible}/>}
+                <Popover width={70} text="Info">
+                  {movieInfo.overview}
+                </Popover>
+              </div>
 
-    </main>
-  </div> : <div className={styles.container}><Loader/></div>
+            </main>
+          </div>
+      ) : <div className={styles.container}><Loader/></div>
+      : null
 }
 
 
