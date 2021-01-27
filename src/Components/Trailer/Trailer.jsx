0da6770d16fa19f0e78e69c83fd9d1f184412a96
Dalnogirl <velikiizevs@gmail.com>
2021-01-27 18:53:12 +0300

    fix: added movieId prop for usage in MovieCard.jsx

--- src/Components/Trailer/Trailer.jsx
+++ src/Components/Trailer/Trailer.jsx
@@ -1,39 +1,37 @@
 import {useLocation} from 'react-router-dom'
 import {useDispatch, useSelector} from 'react-redux'
+import React, {useEffect} from 'react'
 import {urlHelpers} from '../utils/functions/functions'
-import React, {useEffect, useState} from 'react'
 import {setTrailerKey} from '../../redux/headerReducer'
 import {getTrailerKey} from '../../redux/selectors/selectors'
 import styles from './Trailer.module.scss'
 
- const Trailer = () => {
-
+const Trailer = ({callback, movieId}) => {
   const location = useLocation()
   const dispatch = useDispatch()
-  let id = urlHelpers.getId(location)
-
+  const id = urlHelpers.getId(location) || movieId
   useEffect(() => {
     dispatch(setTrailerKey(id))
-    setIsTrailerOnScreen(true)
-    debugger
   }, [id])
-   console.log(location)
+  const trailerKey = useSelector(getTrailerKey)
 
-  let trailerKey = useSelector(getTrailerKey)
-  let [isTrailerOnScreen, setIsTrailerOnScreen] = useState(true)
-  console.log('trailer Key', trailerKey)
-
-  return isTrailerOnScreen &&
-      <div className={styles.iframeContainer}
-           onClick={() => setIsTrailerOnScreen(false)}>
-        {trailerKey && <iframe src={`http://www.youtube.com/embed/${trailerKey}`}
-                 frameBorder="0"
-                 className={styles.trailerIframe}
-
-                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                 allowFullScreen/>}
+  return (
+      <div
+          className={styles.iframeContainer}
+          onClick={() => callback(false)}
+      >
+        {trailerKey
+        && (
+            <iframe
+                src={`http://www.youtube.com/embed/${trailerKey}`}
+                frameBorder="0"
+                className={styles.trailerIframe}
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+            />
+        )}
       </div>
-
+  )
 }
 
 export default Trailer
