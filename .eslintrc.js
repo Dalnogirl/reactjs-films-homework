adffacd530d52056fca0a3a29ffb04ae16ad5b17
Dalnogirl <velikiizevs@gmail.com>
2021-01-27 18:55:13 +0300

    fix: disable hooks deps rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,33 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'es2020': true
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'airbnb', 'airbnb/hooks',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    'extends': [
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'airbnb', 'airbnb/hooks'
-    ],
-    'parserOptions': {
-        'ecmaFeatures': {
-            'jsx': true
-        },
-        'ecmaVersion': 12,
-        'sourceType': 'module'
-    },
-    'plugins': [
-        'react'
-    ],
-    'rules': {
-        'semi': [2, 'never'],
-        'linebreak-style': 0,
-        'object-curly-spacing': 0,
-        'react/jsx-tag-spacing': 0,
-        'react/jsx-filename-extension': 0,
-        'react/jsx-indent': 0,
-        'import/extensions': 0
-    }
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    semi: [2, 'never'],
+    'linebreak-style': 0,
+    'object-curly-spacing': 0,
+    'react/jsx-tag-spacing': 0,
+    'react/jsx-filename-extension': 0,
+    'react/jsx-indent': 0,
+    'react-hooks/exhaustive-deps': 0,
+    indent: 0,
+    'import/extensions': 0,
+    'react/jsx-indent-props': 0,
+  },
 }
