25078be20199692bbc1fe782e772da2dcee48258
Dalnogirl <velikiizevs@gmail.com>
2021-02-08 15:47:10 +0300

    fix: navbar items highlight

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,15 @@
 import React, {useEffect} from 'react'
-import {NavLink} from 'react-router-dom'
+import {NavLink, useLocation} from 'react-router-dom'
 import {useDispatch} from 'react-redux'
 import styles from './Navbar.module.scss'
 import {setGenresObj} from '../../redux/moviesReducer'
 import Dropdown from '../utils/Dropdown/Dropdown'
+import {urlHelpers} from '../utils/functions/functions'
 
 const Navbar = () => {
   const dispatch = useDispatch()
+  const location = useLocation()
+  const filter = urlHelpers.getFilter(location)
   useEffect(() => {
     dispatch(setGenresObj())
   }, [])
@@ -14,22 +17,25 @@ const Navbar = () => {
       <div className={styles.navbar}>
         <NavLink
             to="/?filter=top_rated&page=1"
-            className={styles.navLink}
-            activeClassName={styles.selected}
+            className={filter === 'top_rated'
+                ? styles.selected
+                : styles.navLink}
         >
           Top Rated
         </NavLink>
         <NavLink
             to="/?filter=popular&page=1"
-            className={styles.navLink}
-            activeClassName={styles.selected}
+            className={filter === 'popular'
+                ? styles.selected
+                : styles.navLink}
         >
           Popular
         </NavLink>
         <NavLink
             to="/?filter=upcoming&page=1"
-            className={styles.navLink}
-            activeClassName={styles.selected}
+            className={filter === 'upcoming'
+                ? styles.selected
+                : styles.navLink}
         >
           Upcoming
         </NavLink>
