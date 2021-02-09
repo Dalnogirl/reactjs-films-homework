5d1bf116a4b47cc046cdbdb9b1c2fe5efcd82276
Dalnogirl <velikiizevs@gmail.com>
2021-02-09 18:48:59 +0300

    feat: new actions for current state of the app

--- src/redux/moviesReducer.js
+++ src/redux/moviesReducer.js
@@ -1,17 +1,22 @@
 import {moviesApi} from '../dal/dal'
-import {urlHelpers} from '../Components/utils/functions/functions'
 
 const SET_MOVIES = 'SET_MOVIES'
 const SET_GENRES_OBJ = 'SET_GENRES_OBJ'
 const SET_IS_FETCHING = 'SET_IS_FETCHING'
 const INCREMENT_CURRENT_PAGE = 'INCREMENT_CURRENT_PAGE'
 const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE'
+const SET_CURRENT_GENRE = 'SET_CURRENT_GENRE'
+const SET_CURRENT_FILTER = 'SET_CURRENT_FILTER'
+const SET_TOTAL_RESULTS = 'SET_TOTAL_RESULTS'
 
 const initialState = {
   isFetching: false,
   moviesList: null,
   genresObj: {},
-
+  currentFilter: 'top_rated',
+  currentGenre: null,
+  currentPage: 1,
+  totalResults: null,
 }
 
 function moviesReducer(state = initialState, action) {
@@ -46,18 +51,34 @@ function moviesReducer(state = initialState, action) {
         currentPage: state.currentPage + 1,
       }
     }
+    case SET_CURRENT_FILTER: {
+      return {
+        ...state,
+        currentFilter: action.data,
+      }
+    }
+    case SET_CURRENT_GENRE: {
+      return {
+        ...state,
+        currentGenre: action.data,
+      }
+    }
+    case SET_TOTAL_RESULTS: {
+      return {
+        ...state,
+        totalResults: action.data,
+      }
+    }
 
     default:
       return state
   }
 }
 
-const urlCreatorForAJAX = (location) => {
-  const filter = urlHelpers.getFilter(location)
-  const searchQuery = urlHelpers.getSearchQuery(location)
-  const page = urlHelpers.getPage(location)
-  const genre = urlHelpers.getGenre(location)
-  if (location.pathname === '/search') {
+const urlCreatorForAJAX = ({
+  genre, filter, page = 1, searchQuery,
+}) => {
+  if (searchQuery) {
     return `/search/movie?api_key=0d62501dce3049a65b9d183d8e927cfa&query=${searchQuery}&page=${page}`
   }
   if (genre) {
@@ -70,18 +91,24 @@ export const moviesActions = {
   setGenresObj: (data) => ({type: SET_GENRES_OBJ, data}),
   setMovies: (data) => ({type: SET_MOVIES, data}),
   setIsFetching: (data) => ({type: SET_IS_FETCHING, data}),
-
+  setCurrentFilter: data => ({type: SET_CURRENT_FILTER, data}),
+  setCurrentPage: data => ({type: SET_CURRENT_PAGE, data}),
+  setCurrentGenre: data => ({type: SET_CURRENT_GENRE, data}),
+  setTotalResults: data => ({type: SET_TOTAL_RESULTS, data}),
+  incrementCurrentPage: () => ({type: INCREMENT_CURRENT_PAGE}),
 }
 
 export const setGenresObj = () => (dispatch) => {
-  moviesApi.getGenresObj()
-    .then((data) => dispatch(moviesActions.setGenresObj(data)))
+  moviesApi.getGenresObj().
+      then((data) => dispatch(moviesActions.setGenresObj(data)))
 }
 
-export const setMovies = (location) => async (dispatch) => {
+export const setMovies = (props) => async (dispatch) => {
   dispatch(moviesActions.setIsFetching(true))
-  const data = await moviesApi.getMovies(urlCreatorForAJAX(location))
+  const data = await moviesApi.getMovies(urlCreatorForAJAX(props))
+  props.filter && moviesActions.setCurrentFilter(props.filter)
   dispatch(moviesActions.setMovies(data))
+  dispatch(moviesActions.setTotalResults(data.total_results))
   dispatch(moviesActions.setIsFetching(false))
 }
 
