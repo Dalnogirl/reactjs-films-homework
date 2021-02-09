b4bdcbb6f93b30be3938c9469462752e10b25d1f
Dalnogirl <velikiizevs@gmail.com>
2021-02-09 18:50:26 +0300

    feat: new selectors for getting current state of app

--- src/redux/selectors/selectors.js
+++ src/redux/selectors/selectors.js
@@ -1,18 +1,15 @@
 import {createSelector} from 'reselect'
 
 export const getMovieInfoSelector = (state) => state.headerData.movieInfo
-
 export const getMoviesSelector = (state) => state.moviesData.moviesList
-
 export const getGenresSelector = (state) => state.moviesData.genresObj
-
 export const getIsFetching = (state) => state.moviesData.isFetching
-
 export const getIsHeaderFetching = (state) => state.headerData.isHeaderFetching
-
+export const getCurrentFilter = (state) => state.moviesData.currentFilter
 export const getTrailerKey = (state) => state.headerData.trailerKey
-
 export const getCurrentPage = (state) => state.moviesData.currentPage
+export const getCurrentGenre = (state) => state.moviesData.currentGenre
+export const getTotalResults = (state) => state.moviesData.totalResults
 
 export const getGenresNames = createSelector(getGenresSelector,
-  (genresArray) => genresArray.map((i) => i.name))
+    (genresArray) => genresArray.map((i) => i.name))
