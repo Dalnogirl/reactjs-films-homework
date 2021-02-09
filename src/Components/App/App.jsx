9b8bfbf6b165847780531638617bc2858e4f73b6
Dalnogirl <velikiizevs@gmail.com>
2021-02-09 18:42:05 +0300

    fix: route usage

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import {hot} from 'react-hot-loader'
 import {Provider} from 'react-redux'
-import {BrowserRouter, Redirect, Route} from 'react-router-dom'
+import {
+BrowserRouter, Redirect, Route, Switch,
+} from 'react-router-dom'
 import styles from './App.scss'
 import MovieDetailsPage from '../MovieDetailsPage/MovieDetailsPage'
 import store from '../../redux/store'
@@ -9,24 +11,32 @@ import MoviesGrid from '../MoviesGrid/MoviesGrid'
 import Navbar from '../Navbar/Navbar'
 import Input from '../utils/Input/Input'
 
+const MainSection = () => (
+    <main className={styles.main}>
+      <Navbar/>
+      <MoviesGrid/>
+    </main>
+)
+
 const App = () => (
     <div className={styles.app}>
-      <Route
-          exact
-          path="/"
-          render={() => <Redirect to="/?filter=top_rated&page=1"/>}
-      />
       <header className={styles.header}>
         <h1 className={styles.logo}>FILMS</h1>
         <div className={styles.searchContainer}>
           <Input type="text" placeholder="Search"/>
         </div>
       </header>
-      <MovieDetailsPage/>
-      <main className={styles.main}>
-        <Navbar/>
-        <MoviesGrid/>
-      </main>
+      <Switch>
+        <Route exact path="/" component={MainSection}/>
+        <Route
+path={['/movie/:id', '/movie/:id/search?q=:q']}
+               component={MovieDetailsPage}
+        />
+        <Route path="/search" component={MainSection}/>
+        <Route path="/404" component={() => 'Not Found'}/>
+        <Redirect to="/404"/>
+      </Switch>
+      <Route path="/movie" component={MainSection}/>
     </div>
 )
 
