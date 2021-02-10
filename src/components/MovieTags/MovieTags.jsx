8df61486ec11af89ba48146292020496d239e5b2
Dalnogirl <velikiizevs@gmail.com>
2021-02-10 11:23:00 +0300

    feat: proptypes

--- src/components/MovieTags/MovieTags.jsx
+++ src/components/MovieTags/MovieTags.jsx
@@ -1,37 +1,42 @@
 import React, {useEffect, useState} from 'react'
+import PropTypes from 'prop-types'
 import styles from './MovieTags.module.scss'
 
 const MovieTags = (({genresNames, genresIds, allGenres}) => {
-    let [resultGenres, setResultGenres] = useState([])
+  const [resultGenres, setResultGenres] = useState([])
 
-    useEffect(() => {
-        if (genresNames) {
-            setResultGenres(genresNames)
-        } else {
-            if ('genres' in allGenres)setResultGenres(allGenres.genres.filter(item => genresIds.includes(item.id)))
-        }
-    }, [allGenres, genresNames])
+  useEffect(() => {
+    if (genresNames) {
+      setResultGenres(genresNames)
+    } else if ('genres' in allGenres) {
+      setResultGenres(
+          allGenres.genres.filter((item) => genresIds.includes(item.id)),
+      )
+    }
+  }, [allGenres, genresNames])
 
-    return (
-        <div className={styles.movieTagContainer}>
-            {resultGenres
-                .slice(0, 4)
-                .map((item, index) => (
-                    <div className={styles.movieTag}
-                         key={index}>{item.name}
-                    </div>))}
-        </div>
-    )
+  return (
+      <div className={styles.movieTagContainer}>
+        {resultGenres.slice(0, 4).map((item, index) => (
+            <div
+                className={styles.movieTag}
+                key={index}
+            >
+              {item.name}
+            </div>
+        ))}
+      </div>
+  )
 })
 
+MovieTags.propTypes = {
+  genresNames: PropTypes.arrayOf(PropTypes.string).isRequired,
+  genresIds: PropTypes.arrayOf(PropTypes.number).isRequired,
+  allGenres: PropTypes.shape({
+    genres: PropTypes.arrayOf(PropTypes.shape(
+        {id: PropTypes.number, name: PropTypes.string},
+    )),
+  }).isRequired,
+}
+
 export default React.memo(MovieTags)
-
-
-
-
-
-
-
-
-
-
