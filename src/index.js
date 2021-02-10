4527199b79e8afe885455cbc4f6aaa8a51dd29b3
Dalnogirl <velikiizevs@gmail.com>
2021-02-10 11:52:59 +0300

    fix: minor

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './Components/App/App'
+import AppContainer from './components/App/AppContainer'
 
- ReactDOM.render(<App/>, document.getElementById('app'))
+ ReactDOM.render(<AppContainer/>, document.getElementById('app'))
