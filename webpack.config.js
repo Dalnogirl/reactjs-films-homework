7cddf8d72f589b2f3848b504f3030a00b6907228
Dalnogirl <velikiizevs@gmail.com>
2021-02-10 11:49:04 +0300

    fix: remove console log

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,5 @@ const developmentConfig = require('./config/webpack.dev')
 const productionConfig = require('./config/webpack.prod')
 
 const isDevelopment = process.env.NODE_ENV === 'development'
-console.log(isDevelopment)
 
 module.exports = () => (isDevelopment ? developmentConfig : productionConfig)
