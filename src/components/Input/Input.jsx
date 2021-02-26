788500f5292247be5f02a5a9f604e30f41dbfd77
Dalnogirl <velikiizevs@gmail.com>
2021-02-26 19:12:39 +0300

    feat: submit onEnter

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -9,28 +9,33 @@ const Input = ({ placeholder }) => {
   const onInputChange = (e) => {
     setInputText(e.target.value)
   }
+  const handleKeyDown = (event, inputText) => {
+    if (event.key === 'Enter') {
+      onSubmit(inputText)
+    }
+  }
+  const onSubmit = (inputText) => history.push(`/search?q=${inputText}`)
   const history = useHistory()
-
   return (
     <div className={styles.inputContainer}>
-      <input value={inputText}
-             onChange={(e) => onInputChange(e)}
-             placeholder={placeholder}
-             className={styles.input}
-             data-testid='searchInput'
+      <input
+        value={inputText}
+        onKeyDown={(e) => {
+          handleKeyDown(e, inputText)
+        }}
+        onChange={(e) => onInputChange(e)}
+        placeholder={placeholder}
+        className={styles.input}
+        data-testid="searchInput"
       />
       <div
         onClick={() => {
-          history.push(`/search?q=${inputText}`)
+          onSubmit(inputText)
         }}
         className={styles.navLink}
-        data-testid='searchButton'
+        data-testid="searchButton"
       >
-        <img className={styles.icon}
-             src={icon}
-             alt="Search button"
-
-        />
+        <img className={styles.icon} src={icon} alt="Search button" />
       </div>
     </div>
   )
