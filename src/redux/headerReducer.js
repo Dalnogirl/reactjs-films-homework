4289ef47c6ba19a458f94b5516e84d33140481a6
Dalnogirl <velikiizevs@gmail.com>
2021-02-26 19:10:37 +0300

    fix: refactored thunk -> saga

--- src/redux/headerReducer.js
+++ src/redux/headerReducer.js
@@ -1,8 +1,13 @@
 import { headerAPI } from '../dal/dal'
+import { all, call, put, takeEvery } from '@redux-saga/core/effects'
+import { appActions } from './appReducer'
 
 const GET_MOVIE_INFO = 'headerReducer/GET_MOVIE_INFO'
 const SET_IS_HEADER_FETCHING = 'headerReducer/SET_IS_HEADER_FETCHING'
 const SET_TRAILER_KEY = 'headerReducer/SET_TRAILER_KEY'
+const SAGA_GET_MOVIE_INFO = 'SAGA_GET_MOVIE_INFO'
+const DISPATCH_TRAILER_KEY = 'DISPATCH_TRAILER_KEY'
+const SET_TRAILER_KEY_ERROR = 'SET_TRAILER_KEY_ERROR'
 const initialState = {
   isHeaderFetching: false,
   trailerKey: null,
@@ -22,10 +27,16 @@ const headerReducer = (state = initialState, action) => {
         isHeaderFetching: action.data,
       }
     }
-    case SET_TRAILER_KEY: {
+    case DISPATCH_TRAILER_KEY: {
       return {
         ...state,
         trailerKey: action.data,
+      }
+    }
+    case SET_TRAILER_KEY_ERROR: {
+      return {
+        ...state,
+        errorMessage: action.data,
       }
     }
     default:
@@ -37,25 +48,35 @@ export const headerActions = {
   getMovieInfo: (data) => ({ type: GET_MOVIE_INFO, data }),
   setIsHeaderFetching: (data) => ({ type: SET_IS_HEADER_FETCHING, data }),
   setTrailerKey: (data) => ({ type: SET_TRAILER_KEY, data }),
+  dispatchTrailerKey: (data) => ({ type: DISPATCH_TRAILER_KEY, data }),
+  sagaGetMovieInfo: (data) => ({ type: SAGA_GET_MOVIE_INFO, data }),
+  setTrailerKeyError: (data) => ({ type: SET_TRAILER_KEY_ERROR, data }),
 }
 
-export const getMovieInfo = (movieId) => async (dispatch) => {
-  dispatch(headerActions.setIsHeaderFetching(true))
-  const data = await headerAPI.getMovieInfo(movieId)
-  dispatch(headerActions.getMovieInfo(data))
-  dispatch(headerActions.setIsHeaderFetching(false))
+function* workGetMovieInfo(action) {
+  yield put(headerActions.setIsHeaderFetching(true))
+  const data = yield call(headerAPI.getMovieInfo, action.data)
+  yield put(headerActions.getMovieInfo(data))
+  yield put(headerActions.setIsHeaderFetching(false))
 }
 
-export const setTrailerKey = (movieId) => async (dispatch) => {
-  let key
+export function* watchSetTrailerKey() {
+  yield takeEvery(SET_TRAILER_KEY, setTrailerKey)
+}
+
+export function* watchHeaderActions() {
+  yield all([takeEvery(SAGA_GET_MOVIE_INFO, workGetMovieInfo), takeEvery(SET_TRAILER_KEY, setTrailerKey)])
+}
+
+function* setTrailerKey(action) {
   try {
-    key = await headerAPI.getTrailerKey(movieId)
-    key = key.results[0].key
+    const data = yield call(headerAPI.getTrailerKey, action.data)
+    const key = data.results[0].key
+    yield put(headerActions.dispatchTrailerKey(key))
   } catch (e) {
-    console.warn(e)
+    console.error(e)
+    yield put(appActions.setError(`Something went wrong with ${action.data} trailer 😢`))
   }
-  console.log(key)
-  dispatch(headerActions.setTrailerKey(key))
 }
 
 export default headerReducer
--- src/redux/moviesReducer.js
+++ src/redux/moviesReducer.js
@@ -1,12 +1,16 @@
 import { moviesApi } from '../dal/dal'
+import { all, call, put, takeEvery } from '@redux-saga/core/effects'
+import { appActions } from './appReducer'
 
 export const SET_MOVIES = 'moviesReducer/SET_MOVIES'
+const DISPATCH_MOVIES = 'moviesReducer/DISPATCH_MOVIES'
 const SET_GENRES_OBJ = 'moviesReducer/SET_GENRES_OBJ'
 export const SET_IS_FETCHING = 'moviesReducer/SET_IS_FETCHING'
 const SET_CURRENT_PAGE = 'moviesReducer/SET_CURRENT_PAGE'
 export const SET_CURRENT_GENRE = 'moviesReducer/SET_CURRENT_GENRE'
 const SET_CURRENT_FILTER = 'moviesReducer/SET_CURRENT_FILTER'
 const SET_TOTAL_RESULTS = 'moviesReducer/SET_TOTAL_RESULTS'
+const DISPATCH_GENRES_OBJ = 'moviesReducer/DISPATCH_GENRES_OBJ'
 
 const initialState = {
   isFetching: false,
@@ -20,13 +24,13 @@ const initialState = {
 
 const moviesReducer = (state = initialState, action) => {
   switch (action.type) {
-    case SET_MOVIES: {
+    case DISPATCH_MOVIES: {
       return {
         ...state,
         moviesList: { ...action.data },
       }
     }
-    case SET_GENRES_OBJ: {
+    case DISPATCH_GENRES_OBJ: {
       return {
         ...state,
         genresObj: { ...action.data },
@@ -68,9 +72,7 @@ const moviesReducer = (state = initialState, action) => {
   }
 }
 
-const urlCreatorForAJAX = ({
- genre, filter, page = 1, searchQuery,
-}) => {
+const urlCreatorForAJAX = ({ genre, filter, page = 1, searchQuery }) => {
   if (searchQuery) {
     return `/search/movie?api_key=0d62501dce3049a65b9d183d8e927cfa&query=${searchQuery}&page=${page}`
   }
@@ -83,24 +85,41 @@ const urlCreatorForAJAX = ({
 export const moviesActions = {
   setGenresObj: (data) => ({ type: SET_GENRES_OBJ, data }),
   setMovies: (data) => ({ type: SET_MOVIES, data }),
+  dispatchMovies: (data) => ({ type: DISPATCH_MOVIES, data }),
   setIsFetching: (data) => ({ type: SET_IS_FETCHING, data }),
   setCurrentFilter: (data) => ({ type: SET_CURRENT_FILTER, data }),
   setCurrentPage: (data) => ({ type: SET_CURRENT_PAGE, data }),
   setCurrentGenre: (data) => ({ type: SET_CURRENT_GENRE, data }),
   setTotalResults: (data) => ({ type: SET_TOTAL_RESULTS, data }),
+  dispatchGenresObj: (data) => ({ type: DISPATCH_GENRES_OBJ, data }),
 }
 
-export const setGenresObj = () => (dispatch) => {
-  moviesApi.getGenresObj().then((data) => dispatch(moviesActions.setGenresObj(data)))
+function* setGenresObjWorker() {
+  try {
+    const data = yield call(moviesApi.getGenresObj)
+    yield put(moviesActions.dispatchGenresObj(data))
+  } catch (e) {
+    console.error(e)
+    yield put(appActions.setError('Something went wrong with genres... Please try again later'))
+  }
 }
 
-export const setMovies = (props) => async (dispatch) => {
-  dispatch(moviesActions.setIsFetching(true))
-  const data = await moviesApi.getMovies(urlCreatorForAJAX(props))
-  props.filter && dispatch(moviesActions.setCurrentFilter(props.filter))
-  dispatch(moviesActions.setMovies(data))
-  dispatch(moviesActions.setTotalResults(data.total_results))
-  dispatch(moviesActions.setIsFetching(false))
+export function* watcherMoviesActions() {
+  yield all([takeEvery(SET_MOVIES, setMoviesWorker), takeEvery(SET_GENRES_OBJ, setGenresObjWorker)])
+}
+
+function* setMoviesWorker(action) {
+  try {
+    yield put(moviesActions.setIsFetching(true))
+    const url = urlCreatorForAJAX(action.data)
+    const data = yield call(moviesApi.getMovies, url)
+    yield put(moviesActions.dispatchMovies(data))
+    yield put(moviesActions.setTotalResults(data.total_results))
+    yield put(moviesActions.setIsFetching(false))
+  } catch (e) {
+    console.error(e)
+    yield put(appActions.setError('Something went wrong... Please try again later'))
+  }
 }
 
 export default moviesReducer
