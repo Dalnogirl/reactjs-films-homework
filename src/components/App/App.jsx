b7a50cec5c5c943f81c7eda8d8c1ffca8165c844
Dalnogirl <velikiizevs@gmail.com>
2021-02-26 19:16:30 +0300

    fix: refactored from thunk to saga

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,6 +5,9 @@ import MovieDetailsPage from '../MovieDetailsPage/MovieDetailsPage'
 import MoviesGrid from '../MoviesGrid/MoviesGrid'
 import Navbar from '../Navbar/Navbar'
 import Input from '../Input/Input'
+import { useSelector } from 'react-redux'
+import { getErrorMessage } from '../../redux/selectors/selectors'
+import AlertDialogSlide from '../ErrorAlert/ErrorAlert'
 
 const MainSection = () => (
   <main className={styles.main}>
@@ -13,23 +16,27 @@ const MainSection = () => (
   </main>
 )
 
-const App = () => (
-  <div className={styles.app}>
-    <header className={styles.header}>
-      <h1 className={styles.logo}>FILMS</h1>
-      <div className={styles.searchContainer}>
-        <Input type="text" placeholder="Search" />
-      </div>
-    </header>
-    <Switch>
-      <Route exact path="/" component={MainSection} />
-      <Route path={['/movie/:id', '/movie/:id/search?q=:q']} component={MovieDetailsPage} />
-      <Route path="/search" component={MainSection} />
-      <Route path="/404" component={() => 'Not Found'} />
-      <Redirect to="/404" />
-    </Switch>
-    <Route path="/movie" component={MainSection} />
-  </div>
-)
+const App = () => {
+  const errorMessage = useSelector(getErrorMessage)
+  return (
+    <div className={styles.app}>
+      {errorMessage && <AlertDialogSlide />}
+      <header className={styles.header}>
+        <h1 className={styles.logo}>FILMS</h1>
+        <div className={styles.searchContainer}>
+          <Input type="text" placeholder="Search" />
+        </div>
+      </header>
+      <Switch>
+        <Route exact path="/" component={MainSection} />
+        <Route path={['/movie/:id', '/movie/:id/search?q=:q']} component={MovieDetailsPage} />
+        <Route path="/search" component={MainSection} />
+        <Route path="/404" component={() => 'Not Found'} />
+        <Redirect to="/404" />
+      </Switch>
+      <Route path="/movie" component={MainSection} />
+    </div>
+  )
+}
 
 export default App
